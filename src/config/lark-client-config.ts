5c765f56e9c3d1492a404633569c4953ba303892
涂一乐 <156720897@qq.com>
2026-06-27 17:31:45 +0800

    删除旧实现

--- src/config/lark-client-config.ts
+++
@@ -1,5 +0,0 @@
-export const larkClientConfig = {
-  maxReplyChunkLength: 1400,
-};
-
-export type LarkClientConfig = typeof larkClientConfig;
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./core/logging/logger.js";
-export * from "./lark/index.js";
-export * from "./gateway/bootstrap.js";
-export * from "./codex-app-server/index.js";
